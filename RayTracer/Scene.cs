5134266b0b91208d544a591f56f7403b8ed4248f
TimGoedhuys <32095184+TimGoedhuys@users.noreply.github.com>
2018-05-14 14:59:52 +0200

    axis

--- RayTracer/Scene.cs
+++ RayTracer/Scene.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using Template;
 
 namespace template.Elements
 {
     class Scene
     {
+        public bool Axis = false;
         public List<Primitives.Sphere> PrimitivesList = new List<Primitives.Sphere>();
         public List<Light> LightList = new List<Light>();
 
@@ -16,5 +19,17 @@ namespace template.Elements
         {
             return new Vector3(0,0,0);
         }
+
+        public void toggleaxis(bool TF)
+        {
+            if (TF == true)
+            {
+                Axis = true;
+            }
+            else
+            {
+                Axis = false;
+            }
+        } 
     }
 }
--- RayTracer/game.cs
+++ RayTracer/game.cs
@@ -24,6 +24,8 @@ namespace Template {
         {
             screen.Clear(0);
             screen.Line(screen.width / 2, 0, screen.width / 2, screen.height, 0xff0000);
+            screen.Print("X", screen.width - 25, screen.height /2 + 10, 0xffffff);
+            screen.Print("Z", screen.width / 4 * 3+ 5, screen.height - 20, 0xffffff);
             // Drawing the Red Seperation line in the middle     
         }
 
@@ -34,19 +36,27 @@ namespace Template {
             GL.Disable(EnableCap.Texture2D);
             GL.Clear(ClearBufferMask.DepthBufferBit);
             float FOV =  1;
-
+            Scene Scene1 = new Scene();
+            Scene1.toggleaxis(true);
+            if (Scene1.Axis == true)
+            {
+                GL.Color4(1f, 1f, 1f, 1f);
+                GL.Begin(PrimitiveType.Lines);
+                GL.Vertex2(0.5f, 1); GL.Vertex2(0.5f, -1f);
+                GL.Vertex2(0, 0); GL.Vertex2(1, 0);
+                GL.End();
+            }
             Sphere Sphere1 = new Sphere(new Vector3(1, 1, 0), 0.07f, new Vector3(255,0,0), aspectratio);
             Sphere Sphere2 = new Sphere(new Vector3(0, 1, 0), 0.07f, new Vector3(0, 0, 255), aspectratio);
             Sphere Sphere3 = new Sphere(new Vector3(-1, 1, 0), 0.07f, new Vector3(0, 255,0), aspectratio);
             Camera mainCamera = new Camera(new Vector3(0,0,0), new Vector3(0,0,1), FOV);
             Light LightSource1 = new Light(new Vector3(-1.5f, 0, 0.4f), 255, 255, 0);
-            Scene Scene1 = new Scene();
             Scene1.PrimitivesList.Add(Sphere1);
             Scene1.PrimitivesList.Add(Sphere2);
             Scene1.PrimitivesList.Add(Sphere3);
+  
             Raytracer raytracer = new Raytracer();
             raytracer.Render(mainCamera, Scene1, screen);
-
         }
     }
 
